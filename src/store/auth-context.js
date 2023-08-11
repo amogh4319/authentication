e9e07462667bde66e3b7e910de54524caf157c25
amogh4319 <amogh4319@gmail.com>
2023-08-11 17:27:20 +0530

    auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -12,9 +12,19 @@ export const AuthContextProvider=(props)=>{
     const initialToken=localStorage.getItem('token:');
         const [token,setToken]=useState(initialToken);
         const userIsLogedIn=!!token;
+        setInterval(() => {
+            const currentTime = new Date().getTime();
+            const loginTime = parseInt(localStorage.getItem('loginTime:'));
+            if (currentTime - loginTime > 300000) {
+                logOutHandler();
+            }
+        }, 10000); // Check every 10 seconds
+        
         const logInHandler=(token)=>{
             setToken(token);
-            localStorage.setItem('token:',token);
+            const currentTime = new Date().getTime();
+            localStorage.setItem('token:', token);
+            localStorage.setItem('loginTime:', currentTime);
         }
         const logOutHandler=()=>{
             setToken(null);
